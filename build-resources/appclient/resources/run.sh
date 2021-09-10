65fb3c130341e9b354911997f28ec79226fecab8
MostafaGad-VG <mostafa.gad@virgingates.com>
2021-09-10 21:20:36 +0200

    organizing

--- build-resources/appclient/resources/run.sh
+++ build-resources/appclient/resources/run.sh
@@ -1,1 +1,1 @@
-java -cp osrmt-1.8-SNAPSHOT.jar:libs/* com.osrmt.appclient.reqmanager.RequirementManagerController
+java -cp appClient-module-1.9-SNAPSHOT.jar:libs/* com.osrmt.appclient.reqmanager.RequirementManagerController
