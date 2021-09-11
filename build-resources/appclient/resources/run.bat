c1891502e0931fbbd023e5674f95227ba4f819ab
MostafaGad-VG <mostafa.gad@virgingates.com>
2021-09-11 13:38:13 +0200

    run.bat appClient version

--- build-resources/appclient/resources/run.bat
+++ build-resources/appclient/resources/run.bat
@@ -1,2 +1,2 @@
 Rem javaw -Xms32m -cp * com.osrmt.appclient.reqmanager.RequirementManagerController
-java -Xms32m -cp osrmt-1.8-SNAPSHOT.jar:libs/* com.osrmt.appclient.reqmanager.RequirementManagerController
+java -Xms32m -cp appClient-module-1.9-SNAPSHOT.jar:libs/* com.osrmt.appclient.reqmanager.RequirementManagerController
