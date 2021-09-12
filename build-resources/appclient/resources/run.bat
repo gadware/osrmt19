b0e8a11fa37f317f4a044710b06dac8932d6856a
mostafa.gad <mostafa.a.gad@gmail.com>
2021-09-12 08:39:09 +0200

    fix windows build

--- build-resources/appclient/resources/run.bat
+++ build-resources/appclient/resources/run.bat
@@ -1,2 +1,2 @@
-Rem javaw -Xms32m -cp * com.osrmt.appclient.reqmanager.RequirementManagerController
-java -Xms32m -cp appClient-module-1.9-SNAPSHOT.jar:libs/* com.osrmt.appclient.reqmanager.RequirementManagerController
+Rem java -Xms32m -cp * -cp libs\* com.osrmt.appclient.reqmanager.RequirementManagerController
+java -Xms32m -cp * -cp libs\* com.osrmt.appclient.reqmanager.RequirementManagerController
